0d3148c3d635e084c2e69864aaf128ae65fe9dce
Kishlay Verma <verma.kishlay688@gmail.com>
2020-05-03 12:09:28 +0530

    Add files via upload

---
+++ 2nd-May/solution.java
@@ -0,0 +1,1 @@
+class Solution {    public int numJewelsInStones(String J, String S) {        int sl = S.length();        int jl = J.length();        int i, res = 0;        for (i=0; i<sl; i++) {            if (J.indexOf(S.charAt(i)) != -1)                res++;        }        return res;    }}
