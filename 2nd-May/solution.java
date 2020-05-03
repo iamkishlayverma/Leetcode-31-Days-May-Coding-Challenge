a0d77e8fd35fb6834910b20b194ff354ea3c8bac
Kishlay Verma <verma.kishlay688@gmail.com>
2020-05-03 14:30:47 +0530

    Update solution.java

--- 2nd-May/solution.java
+++ 2nd-May/solution.java
@@ -1,1 +1,12 @@
-class Solution {    public int numJewelsInStones(String J, String S) {        int sl = S.length();        int jl = J.length();        int i, res = 0;        for (i=0; i<sl; i++) {            if (J.indexOf(S.charAt(i)) != -1)                res++;        }        return res;    }}
+public class Solution {
+    public int numJewelsInStones(String J, String S) {
+        int sl = S.length();
+        int jl = J.length();
+        int i, res = 0;
+        for (i=0; i<sl; i++) {
+            if (J.indexOf(S.charAt(i)) != -1)
+                res++;
+        }
+        return res;
+    }
+}
